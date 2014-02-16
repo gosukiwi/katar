dab3b3af08708e46a5373e83c948c58966182a55
Federico Ramirez <fedekiller@gmail.com>
2014-02-16 17:26:34 -0300

    Bump version

--- src/Katar.php
+++ src/Katar.php
@@ -15,7 +15,7 @@ namespace Katar;
  */
 class Katar
 {
-    public static $VERSION = '0.0.3';
+    public static $VERSION = '0.0.4';
 
     public static function autoload($class) {
         if(strpos($class, 'Katar\\') !== 0) {
